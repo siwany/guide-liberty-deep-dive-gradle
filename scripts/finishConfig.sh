745db7915ec59004d19d79eec9a911c01b08a040
Gilbert Kwan <gkwan@ca.ibm.com>
2023-03-27 10:37:35 -0400

    gradle support

--- scripts/finishConfig.sh
+++ scripts/finishConfig.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-if [[ -e ./start/inventory ]]; then
-    rm -fr ./start/inventory
-fi
-mkdir ./start/inventory
 cp -fr ./finish/module-config/* ./start/inventory
 echo Now, you may run following commands to continue the tutorial:
 echo cd start/inventory
-echo mvn liberty:dev -DserverStartTimeout=120
+echo ./gradlew libertyDev
